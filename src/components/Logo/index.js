45794df9e9b2554ef7575df192db0e04fb8e24b4
matheussasm <matheus-michele@hotmail.com>
2019-11-27 20:18:22 -0300

    Logo Login

---
+++ src/components/Logo/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './style.scss';
+
+export default function Cart() {
+  return (
+    <header>
+        <div>
+            <nav>
+                <a href="index.html" class="logo">LILIUM</a>
+            </nav>
+        </div>
+    </header>
+  );
+
+}
