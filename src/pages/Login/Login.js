d6370df1e06c1d5b2ad2172a9a7cbe70eae210cc
matheussasm <matheus-michele@hotmail.com>
2019-11-27 20:47:23 -0300

    Background Image

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Login.scss';
+
+import Logo from '../../components/Logo/index';
+
+export default function Cart() {
+  return (
+    <div className="body">
+        <Logo/>
+        
+    </div>
+  );
+
+}
